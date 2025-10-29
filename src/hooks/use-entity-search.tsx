2246f89629beaf73d8072c6c1c1cbff73871ee7b
Adwitya Hersa <adwihersa@gmail.com>
2025-10-29 17:05:07 +0700

    12: Workflows Pagination

---
+++ src/hooks/use-entity-search.tsx
@@ -0,0 +1,46 @@
+import { PAGINATION } from "@/config/constant";
+import { useEffect, useState } from "react";
+import { set } from "zod";
+
+interface UseEnttiySearchProps<T extends { search: string; page: number }> {
+  params: T;
+  setParams: (params: T) => void;
+  debounceMs?: number;
+}
+
+export function useEntitySearch<T extends { search: string; page: number }>({
+  params,
+  setParams,
+  debounceMs = 300,
+}: UseEnttiySearchProps<T>) {
+  const [localSearch, setLocalSearch] = useState(params.search);
+
+  useEffect(() => {
+    if (localSearch === "" && params.search !== "") {
+      setParams({ ...params, search: "", page: PAGINATION.DEFAULT_PAGE });
+
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      if (localSearch !== params.search) {
+        setParams({
+          ...params,
+          search: localSearch,
+          page: PAGINATION.DEFAULT_PAGE,
+        });
+      }
+    }, debounceMs);
+
+    return () => clearTimeout(timer);
+  }, [localSearch, params, setParams, debounceMs]);
+
+  useEffect(() => {
+    setLocalSearch(params.search);
+  }, [params.search]);
+
+  return {
+    searchValue: localSearch,
+    onSearchChange: setLocalSearch,
+  };
+}
