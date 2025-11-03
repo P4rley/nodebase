8f11926ee65f14d11a03f81d7a383634e8d3a3d4
Adwitya Hersa <adwihersa@gmail.com>
2025-11-03 22:10:34 +0700

    14-workflow-page

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import EditorHeader from "@/features/editor/components/editor-header";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface Props {
   params: Promise<{ workflowId: string }>;
 }
 const Page = async ({ params }: Props) => {
-  const { workflowId } = await params;
+  await requireAuth();
 
-  return <div>Workflow id: {workflowId}</div>;
+  const { workflowId } = await params;
+  prefetchWorkflow(workflowId);
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1">
+            <Editor workflowId={workflowId} />
+          </main>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default Page;
